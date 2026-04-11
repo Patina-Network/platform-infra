e293d951d8a41d85b36a1760db6e9397e83000da
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 04:53:05 -0400

    Add GitHub repos management

---
+++ src/github/repositories/inputs.ts
@@ -0,0 +1,48 @@
+import type { GithubTeamName } from "../teams/inputs.ts";
+
+import { GITHUB_OWNER } from "../inputs.ts";
+
+/**
+ * Can be `public` or `private`.
+ * If your organization is associated with an enterprise account
+ * using GitHub Enterprise Cloud or GitHub Enterprise Server 2.20+,
+ * visibility can also be `internal`.
+ * The `visibility` parameter overrides the `private` parameter.
+ */
+type RepositoryVisibility = "public" | "private" | "internal";
+type GithubTeamReference = `@${typeof GITHUB_OWNER}/${GithubTeamName}`;
+
+type GithubRepository = {
+  existing: boolean;
+  /** GitHub's maintain permission: manage settings, labels, and branches without admin-level destructive access. */
+  maintain: readonly GithubTeamReference[];
+  /** GitHub's push permission: write code and branches, but less access than maintain. */
+  push: readonly GithubTeamReference[];
+  visibility: RepositoryVisibility;
+};
+
+type RepositoryName = string;
+
+export const REPOSITORIES = {
+  "example-repository": {
+    existing: false,
+    visibility: "private",
+    maintain: ["@Patina-Network/admin"],
+    push: ["@Patina-Network/developers"],
+  },
+  "k8s-universe": {
+    existing: true,
+    visibility: "public",
+    maintain: ["@Patina-Network/admin"],
+    push: ["@Patina-Network/developers", "@Patina-Network/infra"],
+  },
+  "platform-infra": {
+    existing: true,
+    visibility: "public",
+    maintain: ["@Patina-Network/admin", "@Patina-Network/infra"],
+    push: ["@Patina-Network/developers"],
+  },
+} as const satisfies Record<RepositoryName, GithubRepository>;
+
+export type GithubRepositoryName = keyof typeof REPOSITORIES;
+export type { GithubTeamReference };
