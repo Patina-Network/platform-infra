8f65b9cff3fd46c515b9837a10bce59c0d29c4c8
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 15:15:26 -0400

    Add explicit Azure creds fwding to Azure provider instead of implicit environment (since that may change at some pt)

--- src/azure/provider.ts
+++ src/azure/provider.ts
@@ -1,7 +1,12 @@
 import * as azure from "@pulumi/azure-native";
 
 import { DEFAULT_REGION } from "@/azure/inputs";
+import { env } from "@/env";
 
 export const provider = new azure.Provider("azure", {
+  clientId: env.azure.clientId,
+  clientSecret: env.azure.clientSecret,
   location: DEFAULT_REGION,
+  subscriptionId: env.azure.subscriptionId,
+  tenantId: env.azure.tenantId,
 });
