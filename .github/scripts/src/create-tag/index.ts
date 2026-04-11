05190a425d86764be116d0dc36ca1c371af264f5
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 00:43:41 -0400

    Setup initial bootstrap (DESC)
    bootstrap Pulumi for Azure and GitHub with Blob state, sops/Key Vault secrets, org membership management, and CI

---
+++ .github/scripts/src/create-tag/index.ts
@@ -0,0 +1,61 @@
+import {
+  EnvClient,
+  EnvClientStrategy,
+  GitHubClient,
+  Utils,
+} from "@tahminator/pipeline";
+
+export async function main() {
+  const envClient = EnvClient.create(EnvClientStrategy.SOPS);
+  const { githubAppAppId, githubAppInstallationId, githubAppPrivateKey } =
+    parseCiEnv(await envClient.readFromEnv("secrets.yaml"));
+
+  const ghClient = await GitHubClient.createWithGithubAppToken({
+    appId: githubAppAppId,
+    installationId: githubAppInstallationId,
+    privateKey: githubAppPrivateKey,
+  });
+
+  await ghClient.createTag({
+    onPreTagCreate: async (tag) => {
+      await Utils.updateAllPackageJsonsWithVersion(tag);
+    },
+  });
+}
+
+function parseCiEnv(ciEnv: Record<string, string>) {
+  const githubAppAppId = (() => {
+    const v = ciEnv["GITHUB_APP_APP_ID"];
+    if (!v) {
+      throw new Error("Missing GITHUB_APP_APP_ID from .env.ci");
+    }
+    return v;
+  })();
+
+  const githubAppInstallationId = (() => {
+    const v = ciEnv["GITHUB_APP_INSTALLATION_ID"];
+    if (!v) {
+      throw new Error("Missing GITHUB_APP_INSTALLATION_ID from .env.ci");
+    }
+    return v;
+  })();
+
+  const githubAppPrivateKey = (() => {
+    const v = ciEnv["GITHUB_APP_PEM_CONTENT"];
+    if (!v) {
+      throw new Error("Missing GITHUB_APP_PRIVATE_KEY from .env.ci");
+    }
+    return v;
+  })();
+
+  return { githubAppAppId, githubAppInstallationId, githubAppPrivateKey };
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
---
+++ .github/scripts/src/pulumi/up.ts
@@ -0,0 +1,56 @@
+import {
+  EnvClient,
+  EnvClientStrategy,
+  PulumiClient,
+  PulumiClientStrategy,
+  Utils,
+} from "@tahminator/pipeline";
+
+export async function main() {
+  const envClient = EnvClient.create(EnvClientStrategy.SOPS);
+  const { azurePulumiLocation, env } = parseCiEnv(
+    await envClient.readFromEnv("secrets.yaml"),
+  );
+
+  const pulumiClient = await PulumiClient.create({
+    strategy: PulumiClientStrategy.AZURE,
+    stackName: "main",
+    workDir: ".",
+    envs: {
+      PULUMI_BACKEND_URL: azurePulumiLocation,
+      ...env,
+    },
+  });
+
+  const res = await pulumiClient.up();
+
+  console.log(`Pulumi has finished reconciling!`);
+  console.log(JSON.stringify(res.summary));
+  if (Utils.Log.isDebug) {
+    console.log(JSON.stringify(res));
+  }
+}
+
+function parseCiEnv(ciEnv: Record<string, string>) {
+  const azurePulumiLocation = (() => {
+    const v = ciEnv["AZURE_PULUMI_LOCATION"];
+    if (!v) {
+      throw new Error("Missing AZURE_PULUMI_LOCATION from .env.ci");
+    }
+    return v;
+  })();
+
+  return {
+    azurePulumiLocation,
+    env: ciEnv,
+  };
+}
+
+main()
+  .then(() => {
+    process.exit(0);
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
