2d073b11b2a474ca3ace464d57cfdcf96165722c
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 18:52:21 -0400

    Wire up existing Pulumi state storage account & blob containers

--- src/azure/storage/index.ts
+++ src/azure/storage/index.ts
@@ -27,3 +27,52 @@ export const platformInfraStorageAccount = new azure.storage.StorageAccount(
   },
   { provider },
 );
+
+export const pulumiStateStorageAccount = new azure.storage.StorageAccount(
+  "pulumi-state-storage-account",
+  {
+    resourceGroupName: "platform-infra",
+    accountName: "platform4pulumi",
+    location: "eastus",
+    kind: azure.storage.Kind.StorageV2,
+    sku: {
+      name: azure.storage.SkuName.Standard_LRS,
+    },
+    accessTier: azure.storage.AccessTier.Hot,
+    allowBlobPublicAccess: false,
+    allowCrossTenantReplication: false,
+    allowSharedKeyAccess: true,
+    defaultToOAuthAuthentication: false,
+    enableHttpsTrafficOnly: true,
+    minimumTlsVersion: azure.storage.MinimumTlsVersion.TLS1_2,
+    dnsEndpointType: azure.storage.DnsEndpointType.Standard,
+    encryption: {
+      keySource: azure.storage.KeySource.Microsoft_Storage,
+      requireInfrastructureEncryption: false,
+      services: {
+        blob: {
+          enabled: true,
+          keyType: azure.storage.KeyType.Account,
+        },
+        file: {
+          enabled: true,
+          keyType: azure.storage.KeyType.Account,
+        },
+      },
+    },
+    networkRuleSet: {
+      bypass: azure.storage.Bypass.AzureServices,
+      defaultAction: azure.storage.DefaultAction.Allow,
+      ipRules: [],
+      resourceAccessRules: [],
+      virtualNetworkRules: [],
+    },
+    publicNetworkAccess: azure.storage.PublicNetworkAccess.Enabled,
+    tags: {},
+  },
+  {
+    provider,
+    import:
+      "/subscriptions/7779681e-36d2-4f42-9289-8160bd1a407d/resourceGroups/platform-infra/providers/Microsoft.Storage/storageAccounts/platform4pulumi",
+  },
+);
