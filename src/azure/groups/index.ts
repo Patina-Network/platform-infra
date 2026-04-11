9cbea8828944d2ed3b2981f1bf81d429e4731590
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 16:02:51 -0400

    Import Azure testing resource group

--- src/azure/groups/index.ts
+++ src/azure/groups/index.ts
@@ -3,6 +3,20 @@ import * as azure from "@pulumi/azure-native";
 import { DEFAULT_REGION } from "@/azure/inputs";
 import { provider } from "@/azure/provider";
 
+export const patinaTestingResourceGroup = new azure.resources.ResourceGroup(
+  "patina-testing-resource-group",
+  {
+    resourceGroupName: "PatinaTesting",
+    location: DEFAULT_REGION,
+    tags: {},
+  },
+  {
+    provider,
+    import:
+      "/subscriptions/7779681e-36d2-4f42-9289-8160bd1a407d/resourceGroups/PatinaTesting",
+  },
+);
+
 export const platformInfraResourceGroup = new azure.resources.ResourceGroup(
   "platform-infra-resource-group",
   {
