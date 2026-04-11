f94ddcfd04a7f34dd0ffd79d1999f53d021d4ec1
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 17:15:06 -0400

    Define new k8s cluster in Azure

---
+++ src/azure/clusters/index.ts
@@ -0,0 +1,98 @@
+import * as azure from "@pulumi/azure-native";
+
+import { patinaTestingK8sResourceGroup } from "@/azure/groups";
+import { DEFAULT_REGION } from "@/azure/inputs";
+import { provider } from "@/azure/provider";
+
+export const k8sUniverseCluster = new azure.containerservice.ManagedCluster(
+  "k8s-manifests-cluster",
+  {
+    resourceGroupName: patinaTestingK8sResourceGroup.name,
+    resourceName: "k8s-manifests",
+    location: DEFAULT_REGION,
+    dnsPrefix: "k8s-manifests-dns",
+    disableLocalAccounts: false,
+    enableRBAC: true,
+    identity: {
+      type: azure.containerservice.ResourceIdentityType.SystemAssigned,
+    },
+    kubernetesVersion: "1.34.4",
+    networkProfile: {
+      ipFamilies: [azure.containerservice.IpFamily.IPv4],
+      loadBalancerProfile: {
+        backendPoolType: "nodeIPConfiguration",
+        managedOutboundIPs: {
+          count: 1,
+        },
+      },
+      loadBalancerSku: "standard",
+      networkDataplane: azure.containerservice.NetworkDataplane.Azure,
+      networkPlugin: azure.containerservice.NetworkPlugin.Azure,
+      networkPluginMode: azure.containerservice.NetworkPluginMode.Overlay,
+      networkPolicy: azure.containerservice.NetworkPolicy.None,
+      outboundType: azure.containerservice.OutboundType.LoadBalancer,
+    },
+    oidcIssuerProfile: {
+      enabled: true,
+    },
+    securityProfile: {
+      imageCleaner: {
+        enabled: true,
+        intervalHours: 168,
+      },
+      workloadIdentity: {
+        enabled: true,
+      },
+    },
+    sku: {
+      name: azure.containerservice.ManagedClusterSKUName.Base,
+      tier: azure.containerservice.ManagedClusterSKUTier.Free,
+    },
+    storageProfile: {
+      diskCSIDriver: {
+        enabled: true,
+      },
+      fileCSIDriver: {
+        enabled: false,
+      },
+      snapshotController: {
+        enabled: false,
+      },
+    },
+    supportPlan:
+      azure.containerservice.KubernetesSupportPlan.KubernetesOfficial,
+    autoUpgradeProfile: {
+      nodeOSUpgradeChannel:
+        azure.containerservice.NodeOSUpgradeChannel.NodeImage,
+      upgradeChannel: azure.containerservice.UpgradeChannel.Patch,
+    },
+    agentPoolProfiles: [
+      {
+        count: 1,
+        enableAutoScaling: false,
+        enableEncryptionAtHost: false,
+        enableFIPS: false,
+        enableNodePublicIP: false,
+        enableUltraSSD: false,
+        kubeletDiskType: azure.containerservice.KubeletDiskType.OS,
+        maxPods: 250,
+        mode: azure.containerservice.AgentPoolMode.System,
+        name: "syspool",
+        osDiskSizeGB: 128,
+        osDiskType: azure.containerservice.OSDiskType.Managed,
+        osSKU: azure.containerservice.OSSKU.Ubuntu,
+        osType: azure.containerservice.OSType.Linux,
+        scaleDownMode: azure.containerservice.ScaleDownMode.Delete,
+        type: azure.containerservice.AgentPoolType.VirtualMachineScaleSets,
+        upgradeSettings: {
+          maxSurge: "10%",
+          maxUnavailable: "0",
+          undrainableNodeBehavior:
+            azure.containerservice.UndrainableNodeBehavior.Schedule,
+        },
+        vmSize: "Standard_DC2as_v5",
+      },
+    ],
+  },
+  { provider },
+);
--- src/azure/index.ts
+++ src/azure/index.ts
@@ -1,3 +1,4 @@
 import "@/azure/identities";
 import "@/azure/groups";
+import "@/azure/clusters";
 import "@/azure/storage";
