9534844ceca9d2caa2fe430bee43b2da9322b576
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 23:23:10 -0400

    Delete k8sDbDataDisk (Azure will auto-provision disks via CSI driver which is very useful)

--- src/azure/disks/index.ts
+++ src/azure/disks/index.ts
@@ -1,21 +1,17 @@
-import * as azure from "@pulumi/azure-native";
-
-import { patinaTestingK8sResourceGroup } from "@/azure/groups";
-import { provider } from "@/azure/provider";
-
-export const k8sDbDataDisk = new azure.compute.Disk(
-  "k8s-db-data-disk",
-  {
-    resourceGroupName: patinaTestingK8sResourceGroup.name,
-    diskName: "k8s-db-data-disk",
-    location: patinaTestingK8sResourceGroup.location,
-    sku: {
-      name: azure.compute.DiskStorageAccountTypes.StandardSSD_LRS,
-    },
-    creationData: {
-      createOption: azure.compute.DiskCreateOption.Empty,
-    },
-    diskSizeGB: 12,
-  },
-  { provider },
-);
+// no longer necessary, but a good example on how to set it up.
+// export const k8sDbDataDisk = new azure.compute.Disk(
+//   "k8s-db-data-disk",
+//   {
+//     resourceGroupName: patinaTestingK8sResourceGroup.name,
+//     diskName: "k8s-db-data-disk",
+//     location: patinaTestingK8sResourceGroup.location,
+//     sku: {
+//       name: azure.compute.DiskStorageAccountTypes.StandardSSD_LRS,
+//     },
+//     creationData: {
+//       createOption: azure.compute.DiskCreateOption.Empty,
+//     },
+//     diskSizeGB: 12,
+//   },
+//   { provider },
+// );
