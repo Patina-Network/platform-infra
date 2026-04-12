4aa3d0d0a7a7f1fc54a735f3399366ed0498c129
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-11 23:42:52 -0400

    Delete imports that are no longer needed

--- src/azure/storage/blob/index.ts
+++ src/azure/storage/blob/index.ts
@@ -17,8 +17,6 @@ export const pulumiStateBlobContainer = new azure.storage.BlobContainer(
   },
   {
     provider,
-    import:
-      "/subscriptions/7779681e-36d2-4f42-9289-8160bd1a407d/resourceGroups/platform-infra/providers/Microsoft.Storage/storageAccounts/platform4pulumi/blobServices/default/containers/pulumi-state",
   },
 );
 
