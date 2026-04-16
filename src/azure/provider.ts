1764650c3c526c94419bf62df4b4cf597ea113f1
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-16 03:26:59 -0400

    Add azuread provider

--- src/azure/provider.ts
+++ src/azure/provider.ts
@@ -1,4 +1,5 @@
 import * as azure from "@pulumi/azure-native";
+import * as azuread from "@pulumi/azuread";
 
 import { DEFAULT_REGION } from "@/azure/inputs";
 import { env } from "@/env";
@@ -8,5 +9,11 @@ export const provider = new azure.Provider("azure", {
   clientSecret: env.azure.clientSecret,
   location: DEFAULT_REGION,
   subscriptionId: env.azure.subscriptionId,
+  tenantId: env.azure.tenantId,
+});
+
+export const azureadProvider = new azuread.Provider("azuread", {
+  clientId: env.azure.clientId,
+  clientSecret: env.azure.clientSecret,
   tenantId: env.azure.tenantId,
 });
