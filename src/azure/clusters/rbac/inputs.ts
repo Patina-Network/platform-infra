f3f0073377bc8004a5036f1ff565bb3c7802931a
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-16 16:09:25 -0400

    Replace AKS RBAC writers with admins (the distinction is unnecessary to our usecase)

--- src/azure/clusters/rbac/inputs.ts
+++ src/azure/clusters/rbac/inputs.ts
@@ -1,21 +1,13 @@
 import type { AzureUserName } from "@/azure/users/inputs";
 
 // This gives you read-only access to the AKS cluster
-export const AKS_CLUSTER_READER_USERS = [
+export const AKS_CLUSTER_READONLY_USERS = [
   "Arshadul Monir",
   "Ray Zhou",
   "Tahmid Ahmed",
 ] as const satisfies AzureUserName[];
 
 // This gives you read-write access to the AKS cluster
-// __NOTE: You must be a reader if you are a writer.__
-export const AKS_CLUSTER_WRITER_USERS = [
+export const AKS_CLUSTER_ADMIN_USERS = [
   "Tahmid Ahmed",
 ] as const satisfies AzureUserName[];
-
-if (
-  AKS_CLUSTER_WRITER_USERS.filter((u) => !AKS_CLUSTER_READER_USERS.includes(u))
-    .length !== 0
-) {
-  throw new Error("All writers must also be readers.");
-}
