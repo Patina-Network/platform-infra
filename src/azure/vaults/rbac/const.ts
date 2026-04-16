c1aa4cac1f24e172b9590b249b4e48ac323479f3
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-16 13:31:30 -0400

    Add comment to AZURE_KEY_VAULT_ROLES indicating Azure UI role name & desc

--- src/azure/vaults/rbac/const.ts
+++ src/azure/vaults/rbac/const.ts
@@ -1,3 +1,8 @@
 export const AZURE_KEY_VAULT_ROLES = {
+  /**
+   * Key Vault Crypto User
+   *
+   * Perform cryptographic operations using keys. Only works for key vaults that use the 'Azure role-based access control' permission model.
+   */
   keyVaultCryptoUser: "12338af0-0e69-4776-bea7-57ae8d297424",
 } as const;
