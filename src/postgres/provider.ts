9d543b161f33a304ba1f8ff0f72b8a2f32937644
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-16 15:26:20 -0400

    Update postgres provider to  depend on k8s manifests cluster

--- src/postgres/provider.ts
+++ src/postgres/provider.ts
@@ -1,11 +1,18 @@
 import * as pg from "@pulumi/postgresql";
 
+import { k8sManifestsCluster } from "@/azure/clusters";
 import { env } from "@/env";
 
-export const provider = new pg.Provider("pg", {
-  database: env.pg.database,
-  host: env.pg.host,
-  port: env.pg.port,
-  username: env.pg.username,
-  password: env.pg.password,
-});
+export const provider = new pg.Provider(
+  "pg",
+  {
+    database: env.pg.database,
+    host: env.pg.host,
+    port: env.pg.port,
+    username: env.pg.username,
+    password: env.pg.password,
+  },
+  {
+    dependsOn: k8sManifestsCluster,
+  },
+);
