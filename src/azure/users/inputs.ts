950401c9ac7361f0bd01e1dbcf85f0f5907a13f1
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-16 01:21:12 -0400

    Add global entry roles within azure users

--- src/azure/users/inputs.ts
+++ src/azure/users/inputs.ts
@@ -1,8 +1,11 @@
 import type { UserArgs } from "@pulumi/azuread";
 
+import type { AzureGlobalRoleName } from "@/azure/users/const";
+
 type AzureUser = {
   /** Set this to objectId if you are importing a user into Pulumi. Then once state is reonciled, delete it. */
   bootstrapObjectId?: string;
+  globalRoles?: readonly AzureGlobalRoleName[];
   mail: UserArgs["mail"];
   mailNickname: UserArgs["mailNickname"];
   preferredLanguage?: UserArgs["preferredLanguage"];
@@ -11,21 +14,26 @@ type AzureUser = {
 
 type AzureUserFullName = string;
 
+// New users should be created in Azure, then bootstrapped to this repository.
+// Then, we can use Pulumi to wire up resources & permissions.
 export const AZURE_USERS = {
   "Arshadul Monir": {
     bootstrapObjectId: "c85509f6-be66-4677-b113-56566f1f4469",
+    globalRoles: ["globalReader"],
     mail: "arshadul@patinanetwork.onmicrosoft.com",
     mailNickname: "arshadul",
     userPrincipalName: "arshadul@patinanetwork.onmicrosoft.com",
   },
   "Kevin Ma": {
     bootstrapObjectId: "93e7f9d0-1a18-4844-b181-db12118cb997",
+    globalRoles: ["globalAdministrator", "userAdministrator", "globalReader"],
     mail: "kevin.ma@patinanetwork.onmicrosoft.com",
     mailNickname: "kevin.ma",
     userPrincipalName: "kevin.ma@patinanetwork.onmicrosoft.com",
   },
   "Ray Zhou": {
     bootstrapObjectId: "bf8318fb-4ea0-48ce-aba7-dcf23fb22470",
+    globalRoles: ["userAdministrator", "globalReader"],
     mail: "ray@patinanetwork.onmicrosoft.com",
     mailNickname: "ray",
     userPrincipalName: "ray@patinanetwork.onmicrosoft.com",
