fdb07a1cb9b1cfbb2f2e706b1ed97266ff022a12
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-24 20:21:19 -0400

    Add temporal polyfill (Node/Bun compat is still half-baked, pollyfill required)

--- src/patches.ts
+++ src/patches.ts
@@ -1,5 +1,12 @@
+import { Temporal } from "@js-temporal/polyfill";
+
 type KeyToTupleObj<T> = { [K in keyof T]: [K, T[K]] };
 type Tuple<T> = KeyToTupleObj<T>[keyof T];
+
+// @ts-expect-error slight mismatches in types are ok.
+// Temporal is in TC4, and been shipped to major browsers like Chrome and Safari
+// so the API is extremely unlikely to change much anymore.
+globalThis.Temporal ??= Temporal;
 
 declare global {
   // generic overrides, from `codebloom`
