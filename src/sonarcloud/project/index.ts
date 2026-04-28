15744ea1f091b9706e20dbe9928a91988e3acb00
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-28 05:03:49 -0400

    Make project ID of Sonarcloud projects scoped to org (IDs must be unique across entire SaaS product, so this should help) (#140)

--- src/sonarcloud/project/index.ts
+++ src/sonarcloud/project/index.ts
@@ -1,10 +1,14 @@
 import * as sonarcloud from "@pulumi/sonarcloud";
 
+import { SONARCLOUD_ORGANIZATION } from "@/sonarcloud/const";
 import { PROJECTS } from "@/sonarcloud/project/inputs";
 import { provider } from "@/sonarcloud/provider";
 
 const getSonarqubeProjectResourceName = (projectName: string) =>
   `sonarcloud-project-${projectName}`;
+
+const getSonarqubeProjectKey = (projectName: string) =>
+  `${SONARCLOUD_ORGANIZATION}_${projectName}` as const;
 
 export const sonarqubeProjects = Object.fromEntries(
   Object.entries(PROJECTS).map(([projectName]) => [
@@ -13,7 +17,7 @@ export const sonarqubeProjects = Object.fromEntries(
       getSonarqubeProjectResourceName(projectName),
       {
         name: projectName,
-        key: projectName,
+        key: getSonarqubeProjectKey(projectName),
       },
       {
         provider,
