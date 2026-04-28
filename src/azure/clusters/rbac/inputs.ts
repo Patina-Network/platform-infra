d7c06d165d20351b3b357980279cce96739a10d4
Tahmid Ahmed <tahmid@tahmid.io>
2026-04-27 21:01:04 -0400

    Provide all Azure users readonly access to k8s cluster

--- src/azure/clusters/rbac/inputs.ts
+++ src/azure/clusters/rbac/inputs.ts
@@ -1,11 +1,10 @@
 import type { AzureIdentityName } from "@/azure/identities";
 
+import { ALL_AZURE_USERS } from "@/azure/rbac";
+
 // This gives you read-only access to the AKS cluster
-export const AKS_CLUSTER_READONLY_USERS = [
-  "Arshadul Monir",
-  "Ray Zhou",
-  "Tahmid Ahmed",
-] as const satisfies AzureIdentityName[];
+export const AKS_CLUSTER_READONLY_USERS =
+  ALL_AZURE_USERS satisfies AzureIdentityName[];
 
 // This gives you read-write access to the AKS cluster
 export const AKS_CLUSTER_ADMIN_USERS = [
