af3968ab80dc553672b37ef541a1d60b5ea58508
Nancy Huang <205217630+naanci@users.noreply.github.com>
2026-08-07 22:08:45 -0400

    TAN-25: restructure DNS records by domain/type

--- src/digitalocean/dns/index.ts
+++ src/digitalocean/dns/index.ts
@@ -1,45 +1,50 @@
 import * as digitalocean from "@pulumi/digitalocean";
 
-import { RECORDS } from "@/digitalocean/dns/inputs";
+import { RECORDS, ROOT, type DnsRecordInput } from "@/digitalocean/dns/inputs";
 import { provider } from "@/digitalocean/provider";
 
-type DigitaloceanDnsRecordMap = Record<string, digitalocean.DnsRecord>;
-type DnsRecord = (typeof RECORDS)[number];
-
-const getDnsRecordDomain = (name: string) =>
-  name.split(".").slice(-2).join(".");
-
-const getDnsRecordHostname = (name: string) => {
-  const domain = getDnsRecordDomain(name);
-  return name === domain ? "@" : name.slice(0, -(domain.length + 1));
-};
-
-const getDnsRecordResourceName = (record: DnsRecord) => {
-  const group = RECORDS.filter(
-    (r) => r.name === record.name && r.type === record.type,
-  );
-  const index = group.indexOf(record);
-  const base = `${record.name.replaceAll(".", "-")}-${record.type}`;
+const getDnsRecordResourceName = (
+  domain: string,
+  hostname: string,
+  type: string,
+  index: number,
+) => {
+  const fqdn = hostname === ROOT ? domain : `${hostname}.${domain}`;
+  const base = `${fqdn.replaceAll(".", "-")}-${type}`;
   return index === 0 ? base : `${base}-${index + 1}`;
 };
 
-export const digitaloceanDnsRecordMap: DigitaloceanDnsRecordMap =
-  Object.fromEntries(
-    RECORDS.map(
-      (record) =>
-        [
-          `${record.name}-${record.type}-${record.value}`,
+export const digitaloceanDnsRecordMap = Object.fromEntries(
+  Object.entries(RECORDS).flatMap(([domain, typeMap]) =>
+    Object.entries(typeMap).flatMap(([type, typedRecords]) => {
+      const records: readonly DnsRecordInput[] = typedRecords;
+
+      return records.map((record) => {
+        const index = records
+          .filter((r) => r.name === record.name)
+          .indexOf(record);
+
+        const resourceName = getDnsRecordResourceName(
+          domain,
+          record.name,
+          type,
+          index,
+        );
+        return [
+          resourceName,
           new digitalocean.DnsRecord(
-            getDnsRecordResourceName(record),
+            resourceName,
             {
-              domain: getDnsRecordDomain(record.name),
-              name: getDnsRecordHostname(record.name),
+              domain,
+              name: record.name,
               ttl: record.ttl,
-              type: record.type,
+              type,
               value: record.value,
             },
             { provider },
           ),
-        ] as const,
-    ),
-  );
+        ] as const;
+      });
+    }),
+  ),
+);
