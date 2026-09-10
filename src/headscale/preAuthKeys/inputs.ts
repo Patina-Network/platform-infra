e5668293219b400bbf644a5ae113151a92cc9a57
Tahmid Ahmed <tahmid@tahmid.io>
2026-09-10 12:57:26 -0400

    TAN-979: add global-cicd user & preauthkey (so cicd can reach internal svcs if needed)

--- src/headscale/preAuthKeys/inputs.ts
+++ src/headscale/preAuthKeys/inputs.ts
@@ -15,4 +15,9 @@ export const PRE_AUTH_KEYS = {
     timeToExpire: "1y",
     generation: 1,
   },
+  "global-cicd": {
+    reusable: true,
+    timeToExpire: "1y",
+    generation: 1,
+  },
 } as const satisfies Record<PreAuthKeyName, PreAuthKey>;
