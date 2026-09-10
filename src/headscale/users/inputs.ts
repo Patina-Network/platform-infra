a8737c775b94466e678ec29087a77ffe07775b89
Tahmid Ahmed <tahmid@tahmid.io>
2026-09-10 10:39:46 -0700

    TAN-979: temporarily re-add proxy-infra (will remove in next pr once state is reconciled) (#263)

--- src/headscale/users/inputs.ts
+++ src/headscale/users/inputs.ts
@@ -21,6 +21,9 @@ export const MACHINE_USERS = {
   "vpn-infra": {
     name: "vpn-infra",
   },
+  "proxy-infra": {
+    name: "proxy-infra",
+  },
 } as const satisfies Record<MachineUserFullName, MachineUser>;
 
 export type MachineUserName = keyof typeof MACHINE_USERS;
